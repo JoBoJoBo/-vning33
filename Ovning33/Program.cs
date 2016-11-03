6c1007b36e97ca8a7b7e3f81eb0cb4ffd1103c93
Johan Strandberg <johan.alexander.strandberg@hotmail.com>
2016-11-03 11:22:40 +0100

    Add project files.

---
+++ Ovning33/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning33
+{
+    class Program
+    {
+       
+              public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            PrintStuff();
+        }
+        public static void PrintStuff()
+        {
+            Console.WriteLine("Funkar denna metod tro?");
+        }
+
+    }
+}
+
